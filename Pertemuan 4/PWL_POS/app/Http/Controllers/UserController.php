8cb7d0b8a933e16bedf3bef21c7a24b4d432f9bd
= <asyamfarhan@gmail.com>
2024-03-13 09:06:14 +0700

    Praktikum-1

---
+++ Pertemuan 4/PWL_POS/app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+
+        // $data = [
+        //     'username' => 'customer 1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // UserModel::insert($data);
+
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data);
+
+        $data = [
+            'level_id' => '2',
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+        ];
+        UserModel::insert($data);
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+}
